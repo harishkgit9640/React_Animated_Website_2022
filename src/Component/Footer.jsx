ab27d1e7e4046e65a44847509488796d70259683
Harish Kumar <harishstudio877@gmail.com>
2023-07-02 23:32:30 +0530

    minor changes

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
         <>
             <footer className='w-100 bg-light text-center'>
-                <p> &copy; CopyRight 2022 All Rights Resolved || <NavLink to="/">HK Solution</NavLink> </p>
+                <p> &copy; copyrights {year} All Rights Resolved Made with 💘 by <NavLink to="/">HK Solution</NavLink> </p>
             </footer>
         </>
     )
